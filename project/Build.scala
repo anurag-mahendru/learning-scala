db5cd3ddb5dc52f9f8a8432d0b5f459fdfc5594f
Anurag Mahendru <anurag.mahendru@glassbeam.com>
2014-07-21 17:57:09 +0530

    First Commit(Shree Ganesh) of new project(A step ahead in scala)

---
+++ project/Build.scala
@@ -0,0 +1,21 @@
+import sbt._
+import sbt.Keys._
+import sbtassembly.Plugin._
+import AssemblyKeys._
+
+object ProjectBuild extends Build {
+
+  lazy val root = Project(
+    id = "root",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "Learning-Scala",
+      organization := "code.hawk",
+      version := "1.0",
+      scalaVersion := "2.10.2",
+      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/",
+      resolvers += "restlet" at "http://maven.restlet.org",
+
+		       		    	      )
+			 )
+}
